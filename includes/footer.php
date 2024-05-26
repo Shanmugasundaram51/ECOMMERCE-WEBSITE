e75736438593b8b85b236417a670a3a48251c88f
Shanmugasundaram51 <143422514+Shanmugasundaram51@users.noreply.github.com>
2024-05-26 12:13:31 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Fashion Studio | All Rights Reserved | Contact Us: +91 9360509515</b></span></div>
+</footer>
+    
